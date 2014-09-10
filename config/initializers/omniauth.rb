d1093bbc20e7747de9ffd8482260f875fb89897b
Alex Kalderimis <alexkalderimis@gmail.com>
2014-09-10 11:48:59 +0100

    Better raven page

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     require 'openid/store/filesystem'
 
     provider :developer unless Rails.env.production?
-    provider :raven, ENV['RAVEN_KEY'], ENV['RAVEN_SECRET']
     provider :openid, :store => OpenID::Store::Filesystem.new('/tmp')
+    provider :raven, ENV['RAVEN_KEY'], ENV['RAVEN_SECRET'], raven_opt: {desc: "Kyuka", msg: "Kyuka needs to access your leave history"}
 end
