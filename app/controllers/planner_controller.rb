3850fe27a3b81df2f154d2c0b989e49d3d6440f6
Alex Kalderimis <alexkalderimis@gmail.com>
2014-09-10 18:57:53 +0100

    Fewer queries processing the planner.

--- app/controllers/planner_controller.rb
+++ app/controllers/planner_controller.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 class PlannerController < ApplicationController
 
     def show
@@ -6,10 +8,18 @@ class PlannerController < ApplicationController
         else
             @range = (Date.current - 2.weeks) .. (Date.current + 2.weeks)
         end
-        @holidays = {}
-        User.all.select{|u| u.has_role? 'user' }.each do |user|
-            days_off = @range.select {|date| user.has_day_off? date }
-            @holidays[user.identifier] = days_off
+        time_periods = TimePeriod.active.overlapping(@range).includes(:user)
+        @users = User.joins(:roles).where(["roles.name = ?", "user"])
+        @holidays = Hash.new
+        @users.each do |u|
+            @holidays[u.identifier] = Set.new
+        end
+
+        time_periods.each do |tp|
+            hols = @holidays[tp.user.identifier]
+            (tp.start_date.to_date .. tp.end_date.to_date).each do |day_off|
+                hols.add day_off
+            end
         end
     end
 end
